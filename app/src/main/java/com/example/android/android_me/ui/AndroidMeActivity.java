6fdbb783d33e58846eb4a9371d5c26b243e3360f
Gabriel Montoya <gabi.montoya@gmail.com>
2017-09-12 23:05:16 +0200

    TFragments.02-Exercise-DisplayThreeFragments - Excersise solved // TODO (1) Create a setter method and class variable to set and store of a list of image resources
    // TODO (2) Create another setter method and variable to track and set the index of the list item to display
    // ex. index = 0 is the first image id in the given list , index 1 is the second, and so on
    
    // TODO (3) If a list of image ids exists, set the image resource to the correct item in that list
    // Otherwise, create a Log statement that indicates that the list was not found
    
    // TODO (4) Set the list of image id's for the head fragment and set the position to the second image in the list
    // TODO (5) Create and display the body and leg BodyPartFragments

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
@@ -38,6 +39,8 @@ public class AndroidMeActivity extends AppCompatActivity {
         BodyPartFragment headFragment=new BodyPartFragment();
 
         // TODO (4) Set the list of image id's for the head fragment and set the position to the second image in the list
+        headFragment.setImagesIds(AndroidImageAssets.getHeads());
+        headFragment.setListIndex(2);
 
         //Use a FragmentManager and transaction to add the fragment to the screen
         FragmentManager fragmentManager=getSupportFragmentManager();
@@ -48,6 +51,20 @@ public class AndroidMeActivity extends AppCompatActivity {
                 .commit();
 
         // TODO (5) Create and display the body and leg BodyPartFragments
+
+
+        BodyPartFragment bodyFragment=new BodyPartFragment();
+        bodyFragment.setImagesIds(AndroidImageAssets.getBodies());
+        bodyFragment.setListIndex(1);
+        fragmentManager.beginTransaction()
+                .add(R.id.body_container,bodyFragment)
+                .commit();
+
+        BodyPartFragment legsFragment=new BodyPartFragment();
+        legsFragment.setImagesIds(AndroidImageAssets.getLegs());
+        fragmentManager.beginTransaction()
+                .add(R.id.legs_container,legsFragment)
+                .commit();
 
     }
 }
