7a8245bb5353992cb71531b3f39d4a4c3c96aa64
Gabriel Montoya <gabi.montoya@gmail.com>
2017-09-14 21:11:18 +0200

    TFragments.05-Exercise-InterfaceCommunication
    // TODO (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
            // The callback is a method named onImageSelected(int position) that contains information about
            // which position on the grid of images a user has clicked
    
    // TODO (2) Override onAttach to make sure that the container activity has implemented the callback
    
    // TODO (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
    
    // This activity is responsible for displaying the master list of all images
    // TODO (4) Implement the MasterListFragment callback, OnImageClickListener
    
    // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -7,9 +7,16 @@ import com.example.android.android_me.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // This activity is responsible for displaying the master list of all images
+// TODO (4) Implement the MasterListFragment callback, OnImageClickListener
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
+
 }
